88abf18848b62e28ab619cf3e107267fc76caa2f
Dixit Vora <dixit.vora@foodvybe.com>
2020-10-28 13:05:26 +0530

    Chore: add the test for model

--- test/unit/todoController.test.js
+++ test/unit/todoController.test.js
@@ -1,7 +1,13 @@
 const TodoController = require('../../controller/testController');
+const TodoModel = require('../../model/todoModel');
+TodoModel.create = jest.fn();
 
 describe('TodoController.createTodo', () => {
   it('Should have a creatTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function');
+  });
+  it('Should call TodoModel.create', () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
